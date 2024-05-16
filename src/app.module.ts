f4988e4bf23efd3fd99d48e34861f5e10233fcdc
Bocharov Alexander <bocharovalexander@MacBook-Pro-2.local>
2024-05-16 13:53:39 +0300

    works

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TelegrafModule } from 'nestjs-telegraf';
+import { BotModule } from './bot/bot.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { TelegrafModule } from 'nestjs-telegraf';
     TelegrafModule.forRoot({
       token: process.env.BOT_TOKEN,
     }),
+    BotModule,
   ],
   controllers: [AppController],
   providers: [AppService],
